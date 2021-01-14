557925f021252170bd696a9a06ccc1b927208778
raayan <raayan.pillai@gemini.com>
2021-01-14 11:03:22 -0500

    Adding .tmux.conf and relevant setup script changes

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@ echo Backing Up Old Configs
 mv $HOME/.zshrc $HOME/.zshrc.bak
 mv $HOME/.vimrc $HOME/.vimrc.bak
 mv $HOME/.gitconfig $HOME/.gitconfig.bak
+mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 
 echo Making linker configs
 echo "source $PWD/config.zshrc" > $HOME/.zshrc
 echo "export DOTFILE_DIR=$PWD" >> $HOME/.zshrc
 echo "source $PWD/config.vimrc" > $HOME/.vimrc
-echo "[include]
-    path = $PWD/config.gitconfig" > $HOME/.gitconfig
+echo "[include]\n\tpath = $PWD/config.gitconfig" > $HOME/.gitconfig
+echo "source-file $PWD/config.tmux.conf" > $HOME/.tmux.conf
 
 echo Done! Restart your shell
 
