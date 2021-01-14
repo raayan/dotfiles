ea2a79048f9b5a882c2a367ce7cdd371d123006e
raayan <raayan.pillai@gemini.com>
2021-01-14 09:38:26 -0500

    Adding .gitconfig and relevant setup script changes

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@ echo Dotfile Directory: $PWD
 echo Backing Up Old Configs
 mv $HOME/.zshrc $HOME/.zshrc.bak
 mv $HOME/.vimrc $HOME/.vimrc.bak
+mv $HOME/.gitconfig $HOME/.gitconfig.bak
 
 echo Making linker configs
 echo "source $PWD/config.zshrc" > $HOME/.zshrc
 echo "export DOTFILE_DIR=$PWD" >> $HOME/.zshrc
 echo "source $PWD/config.vimrc" > $HOME/.vimrc
+echo "[include]
+    path = $PWD/config.gitconfig" > $HOME/.gitconfig
 
 echo Done! Restart your shell
+
