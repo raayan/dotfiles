f1f13c0d14993e02c8f7a01422f3f6d8e9e27745
raayan <raayan@pillai.wtf>
2024-05-12 10:29:01 -0400

    nvim: lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,9 +1,42 @@
 local lsp = require("lsp-zero")
 
+local function on_attach(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ref", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>ren", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    lsp.buffer_autoformat()
+end
+
+local lspconfig = require('lspconfig')
+lspconfig.rust_analyzer.setup {
+    cmd = { "/home/raayan/.cargo/bin/rust-analyzer" },
+    settings = {
+        ['rust-analyzer'] = {
+            cargo = {
+                extraEnv = {
+                    CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev",
+                },
+                extraArgs = { "--profile", "rust-analyzer" },
+            },
+        },
+    },
+    on_attach = on_attach,
+}
+
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'rust_analyzer',
+    -- 'rust_analyzer',
     'lua_ls',
 })
 
@@ -36,22 +69,7 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>ref", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>ren", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-    lsp.buffer_autoformat()
-end)
+lsp.on_attach(on_attach)
 
 lsp.setup()
 
