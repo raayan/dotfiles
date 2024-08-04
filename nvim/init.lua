df9659ce22c619422ce90f6a80dc3dc3a89390c9
raayan <raayan@pillai.wtf>
2024-08-04 13:30:38 -0400

    add nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,6 +120,39 @@ require("lazy").setup({
                 })
             end,
         },
+        {
+            "hrsh7th/nvim-cmp",
+            event = "InsertEnter",
+            dependencies = {
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+            },
+            config = function()
+                local cmp = require("cmp")
+
+                vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+                cmp.setup({
+                    snippet = {
+                        expand = function(args)
+                            vim.snippet.expand(args.body)
+                        end,
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-e>'] = cmp.mapping.abort(),
+                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    }),
+                    sources = cmp.config.sources({
+                        { name = 'nvim_lsp' },
+                    }, {
+                        { name = 'buffer' },
+                    }),
+                })
+            end,
+        },
         { "rust-lang/rust.vim" },
         {
             "mrcjkb/rustaceanvim",
