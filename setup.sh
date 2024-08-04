0459cc56bf43bdd9ec3f5cdc32098fe0516bff07
raayan <raayan@pillai.wtf>
2024-08-04 18:48:40 -0400

    add alacritty config, better setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,34 @@
-echo Dotfile Directory: $PWD
+set -x
 
-echo Backing Up Old Configs
+THIS_DIR=$(dirname $(readlink -f $0))
+
+echo dotfile directory: $PWD
+
+echo backing up old configs
 mv $HOME/.zshrc $HOME/.zshrc.bak
 mv $HOME/.vimrc $HOME/.vimrc.bak
 mv $HOME/.gitconfig $HOME/.gitconfig.bak
 mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 
-echo Making linker configs
-echo "source $PWD/config.zshrc" > $HOME/.zshrc
-echo "export DOTFILE_DIR=$PWD" >> $HOME/.zshrc
-echo "source $PWD/config.vimrc" > $HOME/.vimrc
-echo "[include]\n\tpath = $PWD/config.gitconfig" > $HOME/.gitconfig
-echo "source-file $PWD/config.tmux.conf" > $HOME/.tmux.conf
+echo linking configs
+-- zsh
+echo "source $THIS_DIR/config.zshrc" > $HOME/.zshrc
+echo "export DOTFILE_DIR=$THIS_DIR" >> $HOME/.zshrc
+-- vim
+echo "source $THIS_DIR/config.vimrc" > $HOME/.vimrc
+-- git
+echo "[include]" > $HOME/.gitconfig
+echo "path = $THIS_DIR/config.gitconfig" >> $HOME/.gitconfig
+-- tmux
+echo "source-file $THIS_DIR/config.tmux.conf" > $HOME/.tmux.conf
+-- alacritty
+ALACRITTY_DIR=$HOME/.config/alacritty
+mkdir -p $ALACRITTY_DIR
+ln -sf $THIS_DIR/config.alacritty.toml $ALACRITTY_DIR/alacritty.toml
 
-echo Done! Restart your shell
+echo linking neovim config
+NVIM_DIR=$HOME/.config/nvim
+mkdir -p $NVIM_DIR
+ln -sf $THIS_DIR/nvim/init.lua $NVIM_DIR/init.lua
 
+echo done! restart your shell
--- setup_nvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-NVIM_DIR=$HOME/.config/nvim
-THIS_DIR=$(dirname $(readlink -f $0))
-mkdir -p $NVIM_DIR
-ln -sf $THIS_DIR/nvim/init.lua $NVIM_DIR/init.lua
-ln -sf $THIS_DIR/nvim/lua $NVIM_DIR/
-ln -sf $THIS_DIR/nvim/after $NVIM_DIR/
